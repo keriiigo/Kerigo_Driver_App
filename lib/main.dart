4ccb2ed7bd731d100c180837e5113b7ea80ec5d2
Thanseeh <thanseehabi3@gmail.com>
2024-09-26 22:48:15 +0530

    Theme added

--- lib/main.dart
+++ lib/main.dart
@@ -13,42 +13,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // theme: ThemeData(
-      //   scaffoldBackgroundColor: HexColor('#F7F7F7'),
-      //   fontFamily: 'Satoshi-Medium',
-      //   brightness: Brightness.light,
-      //   textTheme: const TextTheme(
-      //       bodyLarge: TextStyle(
-      //           color: Colors.black,
-      //           fontWeight: FontWeight.w700,
-      //           fontFamily: 'Satoshi'),
-      //       bodyMedium: TextStyle(
-      //           color: Colors.black,
-      //           fontWeight: FontWeight.w400,
-      //           fontFamily: 'Satoshi-Medium'),
-      //       bodySmall: TextStyle(
-      //           fontFamily: 'Satoshi-Light',
-      //           color: Colors.black,
-      //           fontWeight: FontWeight.normal)),
-      // ),
-      // darkTheme: ThemeData(
-      //   brightness: Brightness.dark,
-      //   scaffoldBackgroundColor: Colors.black,
-      //   textTheme: const TextTheme(
-      //     bodyLarge: TextStyle(
-      //         fontFamily: 'Satoshi',
-      //         color: Colors.white,
-      //         fontWeight: FontWeight.w700),
-      //     bodyMedium: TextStyle(
-      //         fontFamily: 'Satoshi-Medium',
-      //         color: Colors.white,
-      //         fontWeight: FontWeight.w600),
-      //     bodySmall: TextStyle(
-      //         fontFamily: 'Satoshi-Light',
-      //         color: Colors.white,
-      //         fontWeight: FontWeight.w400),
-      //   ),
-      // ),
+      theme: ThemeData(
+        scaffoldBackgroundColor: HexColor('#F7F7F7'),
+        fontFamily: 'Satoshi-Medium',
+        brightness: Brightness.light,
+        textTheme: const TextTheme(
+            bodyLarge: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Satoshi'),
+            bodyMedium: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w400,
+                fontFamily: 'Satoshi-Medium'),
+            bodySmall: TextStyle(
+                fontFamily: 'Satoshi-Light',
+                color: Colors.black,
+                fontWeight: FontWeight.normal)),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.black,
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(
+              fontFamily: 'Satoshi',
+              color: Colors.white,
+              fontWeight: FontWeight.w700),
+          bodyMedium: TextStyle(
+              fontFamily: 'Satoshi-Medium',
+              color: Colors.white,
+              fontWeight: FontWeight.w600),
+          bodySmall: TextStyle(
+              fontFamily: 'Satoshi-Light',
+              color: Colors.white,
+              fontWeight: FontWeight.w400),
+        ),
+      ),
       themeMode: ThemeMode.system,
       home: LocationPermissionScreen(),
     );
